32c399fec3109f12de037ffad81882083b17e686
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2021-10-13 11:17:29 -0400

    fiddling around to get performance up for ros2

--- include/metavision_ros_driver/event_publisher_ros2.h
+++ include/metavision_ros_driver/event_publisher_ros2.h
@@ -16,12 +16,14 @@
 #ifndef METAVISION_ROS_DRIVER__EVENT_PUBLISHER_ROS2_H_
 #define METAVISION_ROS_DRIVER__EVENT_PUBLISHER_ROS2_H_
 
+#include <chrono>
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
 #include <string>
 
 #include "metavision_ros_driver/callback_handler.h"
 #include "metavision_ros_driver/metavision_wrapper.h"
+using namespace std::chrono;
 
 namespace metavision_ros_driver
 {
@@ -32,13 +34,40 @@ public:
   EventPublisherROS2(
     rclcpp::Node * node, const std::shared_ptr<MetavisionWrapper> & wrapper,
     const std::string & frameId)
-  : node_(node), wrapper_(wrapper), messageTimeThreshold_(0, 0), frameId_(frameId)
+  : node_(node), wrapper_(wrapper), messageTimeThreshold_(0, 0), dt_(0), frameId_(frameId)
   {
     messageTimeThreshold_ = rclcpp::Duration::from_nanoseconds(
       (uint64_t)(1e9 * node->declare_parameter<double>("message_time_threshold", 10e-6)));
+#if 0    
     const int qs = node->declare_parameter<int>("send_queue_size", 1000);
+    rmw_qos_profile_t prof = rmw_qos_profile_default;
+    prof.history = RMW_QOS_POLICY_HISTORY_KEEP_LAST;
+    prof.depth = qs;  // keep at most this number of images
 
+    prof.reliability = RMW_QOS_POLICY_RELIABILITY_SYSTEM_DEFAULT;
+    //prof.reliability = RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT;
+
+    prof.durability = RMW_QOS_POLICY_DURABILITY_VOLATILE;  // sender does not have to store
+    //prof.durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL;
+    //prof.durability = RMW_QOS_POLICY_DURABILITY_SYSTEM_DEFAULT;
+    prof.deadline.sec = 5;  // max expect time between msgs pub
+    prof.deadline.nsec = 0;
+
+    prof.lifespan.sec = 1;  // how long until msg are considered expired
+    prof.lifespan.nsec = 0;
+
+    prof.liveliness_lease_duration.sec = 10;  // time to declare client dead
+    prof.liveliness_lease_duration.nsec = 0;
+#endif
+
+#if 1
+    //rclcpp::QoS qosProf(rclcpp::QoSInitialization::from_rmw(prof), prof);
+    auto qosProf = rclcpp::QoS(rclcpp::KeepLast(0)).best_effort().durability_volatile();
+    startTime_ = high_resolution_clock::now();
+    pub_ = node->create_publisher<MsgType>("~/events", qosProf);
+#else
     pub_ = node->create_publisher<MsgType>("~/events", qs);
+#endif
 
     width_ = wrapper_->getWidth();
     height_ = wrapper_->getHeight();
@@ -85,8 +114,25 @@ public:
         msg_->header.stamp = t_msg;
         wrapper_->updateEventsSent(events.size());
         wrapper_->updateMsgsSent(1);
-        // the move() will reset msg_ and transfer ownership
-        pub_->publish(std::move(msg_));
+        auto t_start = high_resolution_clock::now();
+        if (true) {
+          // the move() will reset msg_ and transfer ownership
+          pub_->publish(std::move(msg_));
+        } else {
+          msg_.reset();
+        }
+        auto t_stop = high_resolution_clock::now();
+        dt_ = dt_ + duration_cast<microseconds>(t_stop - t_start);
+        msgCnt_++;
+      }
+      if (msgCnt_ >= 100) {
+        auto t_now = high_resolution_clock::now();
+        auto dt_tot = duration_cast<microseconds>(t_now - startTime_).count();
+        std::cout << "call duration [us]: " << dt_.count() / msgCnt_
+                  << " rate: " << (msgCnt_ * 1e6) / dt_tot << std::endl;
+        msgCnt_ = 0;
+        dt_ = microseconds::zero();
+        startTime_ = t_now;
       }
     } else {
       // no subscribers, just gather event statistics
@@ -107,11 +153,13 @@ private:
   typename rclcpp::Publisher<MsgType>::SharedPtr pub_;
   std::unique_ptr<MsgType> msg_;
   rclcpp::Duration messageTimeThreshold_;  // duration for triggering a message
-
+  microseconds dt_;                        // total time spent in ros calls (perf debugging)
+  high_resolution_clock::time_point startTime_;
   uint64_t t0_{0};  // time base
   int width_;       // image width
   int height_;      // image height
   std::string frameId_;
+  size_t msgCnt_{0};
 };
 }  // namespace metavision_ros_driver
 #endif  // METAVISION_ROS_DRIVER__EVENT_PUBLISHER_ROS2_H_
--- launch/driver_node.launch.py
+++ launch/driver_node.launch.py
@@ -40,8 +40,8 @@ def launch_setup(context, *args, **kwargs):
                      'bias_file': bias_dir + 'silky_ev_cam.bias',
                      'camerainfo_url': '',
                      'frame_id': '',
-                     'message_time_threshold': 100.0e-6,
-                     'send_queue_size': 1500}],
+                     'message_time_threshold': 1e-4,
+                     'send_queue_size': 1000}],
                 remappings=[
                     ('~/events', cam_str + '/events')])
     return [node]
