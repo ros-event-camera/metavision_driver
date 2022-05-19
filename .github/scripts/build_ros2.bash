8e86684b958141482e6ec887461bb3a1d01a371d
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2022-05-19 15:54:48 -0400

    work in progress: continuing integration

---
+++ .github/scripts/build_ros2.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+# set up ROS
+distros=('foxy', 'galactic', 'humble')
+
+#
+# probe for the ROS2 distro
+#
+for distro in "${distros[@]}"
+do
+    echo "probing for ${distro}"
+    if [[ -f "/opt/ros/${distro}/setup.bash" ]]; then
+	source /opt/ros/${distro}/setup.bash
+	echo "found distro: ${distro}"
+	break
+    fi
+done
+
+# run wstool to bring in the additional repositories required
+wstool init src ./src/metavision_ros_driver/metavision_ros_driver.rosinstall
+
+# build
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
