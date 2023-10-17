0dd44164c0dad8ca30b325b7db1e80bace6c6d06
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2023-10-17 05:27:02 -0400

    reformat python code as "black" and fix import order

--- launch/driver_composition.launch.py
+++ launch/driver_composition.launch.py
@@ -15,55 +15,60 @@
 #
 #
 
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
 import launch
-from launch_ros.actions import ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
-from launch.substitutions import LaunchConfiguration as LaunchConfig
 from launch.actions import DeclareLaunchArgument as LaunchArg
 from launch.actions import OpaqueFunction
-from ament_index_python.packages import get_package_share_directory
-import os
+from launch.substitutions import LaunchConfiguration as LaunchConfig
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
 
 
 def launch_setup(context, *args, **kwargs):
     """Create composable node."""
-    cam_name = LaunchConfig('camera_name')
+    cam_name = LaunchConfig("camera_name")
     cam_str = cam_name.perform(context)
-    pkg_name = 'metavision_driver'
+    pkg_name = "metavision_driver"
     share_dir = get_package_share_directory(pkg_name)
-    trigger_config = os.path.join(share_dir, 'config', 'trigger_pins.yaml')
-    bias_config = os.path.join(share_dir, 'config', 'silky_ev_cam.bias')
+    trigger_config = os.path.join(share_dir, "config", "trigger_pins.yaml")
+    bias_config = os.path.join(share_dir, "config", "silky_ev_cam.bias")
     container = ComposableNodeContainer(
-            name='metavision_driver_container',
-            namespace='',
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
-                ComposableNode(
-                    package='metavision_driver',
-                    plugin='metavision_driver::DriverROS2',
-                    name=cam_name,
-                    parameters=[
-                        trigger_config,  # loads the whole file
-                        {'use_multithreading': False,
-                         'bias_file': bias_config,
-                         'camerainfo_url': '',
-                         'frame_id': '',
-                         'event_message_time_threshold': 1.0e-3}],
-                    remappings=[
-                        ('~/events', cam_str + '/events')],
-                    extra_arguments=[{'use_intra_process_comms': True}],
-                )
-            ],
-            output='screen',
+        name="metavision_driver_container",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="metavision_driver",
+                plugin="metavision_driver::DriverROS2",
+                name=cam_name,
+                parameters=[
+                    trigger_config,  # loads the whole file
+                    {
+                        "use_multithreading": False,
+                        "bias_file": bias_config,
+                        "camerainfo_url": "",
+                        "frame_id": "",
+                        "event_message_time_threshold": 1.0e-3,
+                    },
+                ],
+                remappings=[("~/events", cam_str + "/events")],
+                extra_arguments=[{"use_intra_process_comms": True}],
+            )
+        ],
+        output="screen",
     )
     return [container]
 
 
 def generate_launch_description():
     """Create composable node by calling opaque function."""
-    return launch.LaunchDescription([
-        LaunchArg('camera_name', default_value=['event_camera'],
-                  description='camera name'),
-        OpaqueFunction(function=launch_setup)
-        ])
+    return launch.LaunchDescription(
+        [
+            LaunchArg("camera_name", default_value=["event_camera"], description="camera name"),
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
--- launch/recording_driver.launch.py
+++ launch/recording_driver.launch.py
@@ -16,63 +16,67 @@
 #
 
 import launch
-from launch_ros.actions import ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
-from launch.substitutions import LaunchConfiguration as LaunchConfig
 from launch.actions import DeclareLaunchArgument as LaunchArg
 from launch.actions import OpaqueFunction
+from launch.substitutions import LaunchConfiguration as LaunchConfig
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
 
 
 def launch_setup(context, *args, **kwargs):
     """Create composable node."""
-    cam_name = LaunchConfig('camera_name')
+    cam_name = LaunchConfig("camera_name")
     cam_str = cam_name.perform(context)
     container = ComposableNodeContainer(
-            name='metavision_driver_container',
-            namespace='',
-            package='rclcpp_components',
-            executable='component_container',
-            # prefix=['xterm -e gdb -ex run --args'],
-            composable_node_descriptions=[
-                ComposableNode(
-                    package='metavision_driver',
-                    plugin='metavision_driver::DriverROS2',
-                    name=cam_name,
-                    parameters=[
-                        {'use_multithreading': False,
-                         'statistics_print_interval': 2.0,
-                         'camerainfo_url': '',
-                         'frame_id': '',
-                         'event_message_time_threshold': 1.0e-3}],
-                    remappings=[
-                        ('~/events', cam_str + '/events')],
-                    extra_arguments=[{'use_intra_process_comms': True}],
-                ),
-                ComposableNode(
-                    package='rosbag2_composable_recorder',
-                    plugin='rosbag2_composable_recorder::ComposableRecorder',
-                    name="recorder",
-                    parameters=[{'topics': ['/event_camera/events'],
-                                 'bag_name': LaunchConfig('bag'),
-                                 'bag_prefix': LaunchConfig('bag_prefix')}],
-                    remappings=[
-                        ('~/events', cam_str + '/events')],
-                    extra_arguments=[{'use_intra_process_comms': True}],
-                )
-            ],
-            output='screen',
+        name="metavision_driver_container",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container",
+        # prefix=['xterm -e gdb -ex run --args'],
+        composable_node_descriptions=[
+            ComposableNode(
+                package="metavision_driver",
+                plugin="metavision_driver::DriverROS2",
+                name=cam_name,
+                parameters=[
+                    {
+                        "use_multithreading": False,
+                        "statistics_print_interval": 2.0,
+                        "camerainfo_url": "",
+                        "frame_id": "",
+                        "event_message_time_threshold": 1.0e-3,
+                    }
+                ],
+                remappings=[("~/events", cam_str + "/events")],
+                extra_arguments=[{"use_intra_process_comms": True}],
+            ),
+            ComposableNode(
+                package="rosbag2_composable_recorder",
+                plugin="rosbag2_composable_recorder::ComposableRecorder",
+                name="recorder",
+                parameters=[
+                    {
+                        "topics": ["/event_camera/events"],
+                        "bag_name": LaunchConfig("bag"),
+                        "bag_prefix": LaunchConfig("bag_prefix"),
+                    }
+                ],
+                remappings=[("~/events", cam_str + "/events")],
+                extra_arguments=[{"use_intra_process_comms": True}],
+            ),
+        ],
+        output="screen",
     )
     return [container]
 
 
 def generate_launch_description():
     """Create composable node by calling opaque function."""
-    return launch.LaunchDescription([
-        LaunchArg('camera_name', default_value=['event_camera'],
-                  description='camera name'),
-        LaunchArg('bag', default_value=[''],
-                  description='name of output bag'),
-        LaunchArg('bag_prefix', default_value=['events_'],
-                  description='prefix of output bag'),
-        OpaqueFunction(function=launch_setup)
-        ])
+    return launch.LaunchDescription(
+        [
+            LaunchArg("camera_name", default_value=["event_camera"], description="camera name"),
+            LaunchArg("bag", default_value=[""], description="name of output bag"),
+            LaunchArg("bag_prefix", default_value=["events_"], description="prefix of output bag"),
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
