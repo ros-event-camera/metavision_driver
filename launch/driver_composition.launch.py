babcc5e35c5bd622e7db67fc98e76fbabce25ec8
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2025-12-05 19:11:22 -0500

    improved launch files

--- launch/driver_composition.launch.py
+++ launch/driver_composition.launch.py
@@ -16,51 +16,93 @@
 #
 
 
-import os
-
-from ament_index_python.packages import get_package_share_directory
 import launch
 from launch.actions import DeclareLaunchArgument as LaunchArg
 from launch.actions import OpaqueFunction
+from launch.actions import SetEnvironmentVariable
+from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration as LaunchConfig
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 
+fixed_params = {
+    "use_multithreading": False,
+    "bias_file": "",
+    "camerainfo_url": "",
+    "frame_id": "",
+    "event_message_time_threshold": 1.0e-3,
+}
+
+
+def make_renderer(camera):
+    return ComposableNode(
+        package="event_camera_renderer",
+        plugin="event_camera_renderer::Renderer",
+        name=camera + "_renderer",
+        parameters=[{"fps": 25.0}],
+        remappings=[("~/events", camera + "/events")],
+        extra_arguments=[{"use_intra_process_comms": True}],
+    )
+
+
+def make_fibar(camera):
+    return ComposableNode(
+        package="event_image_reconstruction_fibar",
+        plugin="event_image_reconstruction_fibar::Fibar",
+        name=camera + "_fibar",
+        parameters=[{"fps": 25.0}],
+        remappings=[("~/events", camera + "/events")],
+        extra_arguments=[{"use_intra_process_comms": True}],
+    )
+
+
+def make_camera(camera, params, remappings):
+    return ComposableNode(
+        package="metavision_driver",
+        plugin="metavision_driver::DriverROS2",
+        name=camera,
+        parameters=params,
+        remappings=remappings,
+        extra_arguments=[{"use_intra_process_comms": True}],
+    )
+
+
 def launch_setup(context, *args, **kwargs):
     """Create composable node."""
-    cam_name = LaunchConfig("camera_name")
-    cam_str = cam_name.perform(context)
-    pkg_name = "metavision_driver"
-    share_dir = get_package_share_directory(pkg_name)
-    trigger_config = os.path.join(share_dir, "config", "trigger_pins.yaml")
-    bias_config = os.path.join(share_dir, "config", "silky_ev_cam.bias")
+    camera = LaunchConfig("camera_name").perform(context)
+    params = [
+        {
+            "serial": LaunchConfig("serial").perform(context),
+            "settings": LaunchConfig("settings").perform(context),
+        }
+    ]
+    remappings = []
+    nodes = [make_camera(camera, params, remappings)]
+
+    if IfCondition(LaunchConfig("with_renderer")).evaluate(context):
+        nodes += [make_renderer(camera)]
+    if IfCondition(LaunchConfig("with_fibar")).evaluate(context):
+        nodes += [make_fibar(camera)]
     container = ComposableNodeContainer(
         name="metavision_driver_container",
         namespace="",
         package="rclcpp_components",
-        executable="component_container",
-        composable_node_descriptions=[
-            ComposableNode(
-                package="metavision_driver",
-                plugin="metavision_driver::DriverROS2",
-                name=cam_name,
-                parameters=[
-                    trigger_config,  # loads the whole file
-                    {
-                        "use_multithreading": False,
-                        "bias_file": bias_config,
-                        "camerainfo_url": "",
-                        "frame_id": "",
-                        "event_message_time_threshold": 1.0e-3,
-                    },
-                ],
-                remappings=[("~/events", cam_str + "/events")],
-                extra_arguments=[{"use_intra_process_comms": True}],
-            )
-        ],
+        executable="component_container_isolated",
+        composable_node_descriptions=nodes,
         output="screen",
     )
+
+    debug_with_libasan = False
+    if debug_with_libasan:
+        preload = SetEnvironmentVariable(
+            name="LD_PRELOAD", value="/usr/lib/gcc/x86_64-linux-gnu/13/libasan.so"
+        )
+        asan_options = SetEnvironmentVariable(
+            name="ASAN_OPTIONS", value="new_delete_type_mismatch=0"
+        )
+        return [preload, asan_options, container]
+
     return [container]
 
 
@@ -68,7 +110,36 @@ def generate_launch_description():
     """Create composable node by calling opaque function."""
     return launch.LaunchDescription(
         [
-            LaunchArg("camera_name", default_value=["event_camera"], description="camera name"),
+            LaunchArg(
+                "camera_name",
+                default_value=["event_camera"],
+                description="name of camera",
+            ),
+            LaunchArg(
+                "serial",
+                default_value=["4110030785"],
+                description="serial number of camera",
+            ),
+            LaunchArg(
+                "settings",
+                default_value=[""],
+                description="settings file for camera",
+            ),
+            LaunchArg(
+                "fps",
+                default_value=["fps"],
+                description="renderer and fibar frame rate in Hz",
+            ),
+            LaunchArg(
+                "with_renderer",
+                default_value="false",
+                description="if renderers should be started as well",
+            ),
+            LaunchArg(
+                "with_fibar",
+                default_value="false",
+                description="if fibar reconstruction should be started as well",
+            ),
             OpaqueFunction(function=launch_setup),
         ]
     )
